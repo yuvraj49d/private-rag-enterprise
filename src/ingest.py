c421e7c9ee83b45eca1c17fbc14165e7d473878e
Yuvraj Vilas Deshmukh <60032346+yuvraj49d@users.noreply.github.com>
2026-06-11 05:00:26 +0000

    Changing print statements and adding Dockerfile

--- src/ingest.py
+++ src/ingest.py
@@ -2,12 +2,13 @@ from langchain_community.documents_loaders import DirectoryLoader, UnstructuredP
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from src.config import settings
 import os
+from src.logger import logger
 
 def load_and_chunk_documents():
     """Load PDFs from local and split them securely"""
     if not os.path.exists(settings.DATA_DIR):
         os.makedirs(settings.DATA_DIR)
-        print(f"Created {settings.DATA_DIR}. Drop the PDFs here")
+        logger.warning(f"Created {settings.DATA_DIR}. Drop the PDFs here")
         return []
     
     # Strict local parsing
@@ -18,6 +19,7 @@ def load_and_chunk_documents():
     )
 
     documents = loader.load()
+    logger.info(f"Successfully extracted text from {len(documents)} documents.")
 
     #Splitting text but preserving sentence context
     text_splitter = RecursiveCharacterTextSplitter(
@@ -26,4 +28,5 @@ def load_and_chunk_documents():
     )
 
     chunks = text_splitter.split_documents(documents)
+    logger.info(f"Generated {len(chunks)} text chunks for indexing.")
     return chunks
--- src/vector_store.py
+++ src/vector_store.py
@@ -2,30 +2,32 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from src.config import settings
 from src.ingest import load_and_chunk_documents
+from src.logger import logger
 
 def build_local_vector_db():
     """Generates local embeddings and saves them to a secure disk database."""
-    print("Initializing local HuggingFace embeddings...")
+    logger.info("Initializing local HuggingFace embeddings...")
     # Computes purely on Local CPU/GPU via PyTorch
     embeddings = HuggingFaceEmbeddings (model_name=settings.EMBEDDING_MODEL_NAME)
 
     chunks = load_and_chunk_documents()
     if not chunks:
-        print("No documents found to index.")
+        logger.error("No documents found to index.")
         return None
 
-    print(f"Indexing {len(chunks)} chunks into local ChromaDB...")
+    logger.info(f"Indexing {len(chunks)} chunks into local ChromaDB...")
     vector_db = Chroma.from_documents(
         documents=chunks,
         embedding=embeddings,
         persist_directory=settings.DB_DIR
     )
 
-    print(f"Database successfully saved locally at: {settings.DB_DIR}")
+    logger.info(f"Database successfully saved locally at: {settings.DB_DIR}")
     return vector_db
 
 def get_local_retriever():
     """Loads the pre-built local database for quick querying."""
+    logger.info("Connecting to persistent local ChromaDB instance...")
     embeddings = HuggingFaceEmbeddings (model_name=settings.EMBEDDING_MODEL_NAME)
     vector_db = Chroma (
         persist_directory=settings.DB_DIR,
